a7e545c6d8d44839679bdd452cd3ad1175a94e47
Gustavo Araujo <gustavobr1989@gmail.com>
2017-05-24 16:11:00 -0300

    Primeiro commit.

--- xamarin.geolocation/Position.cs
+++ xamarin.geolocation/Position.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
 namespace xamarin.geolocation
 {
-	public class Position
+	public class Position : INotifyPropertyChanged
 	{
-		public Position()
-		{
+		private DateTimeOffset timestamp;
+		private double latitude;
+		private double longitude;
+
+		public DateTimeOffset Timestamp 
+		{ 
+			get { return timestamp; } 
+			set 
+			{
+				timestamp = value;
+
+				if (PropertyChanged != null) 
+				{
+					PropertyChanged(this, new PropertyChangedEventArgs("Timestamp"));
+				}
+			} 
 		}
+
+		public double Latitude 
+		{
+			get { return latitude; }
+			set
+			{
+				latitude = value;
+
+				if (PropertyChanged != null)
+				{
+					PropertyChanged(this, new PropertyChangedEventArgs("Latitude"));
+				}
+			}
+		}
+
+		public double Longitude 
+		{
+			get { return longitude; }
+			set
+			{
+				longitude = value;
+
+				if (PropertyChanged != null)
+				{
+					PropertyChanged(this, new PropertyChangedEventArgs("Longitude"));
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
--- xamarin.geolocation/xamarin.geolocationPage.xaml.cs
+++ xamarin.geolocation/xamarin.geolocationPage.xaml.cs
@@ -1,12 +1,42 @@
 ﻿using Xamarin.Forms;
+using System;
+using System.Diagnostics;
+using Plugin.Geolocator;
 
 namespace xamarin.geolocation
 {
 	public partial class xamarin_geolocationPage : ContentPage
 	{
+		public string Teste { get; set; } = "Teste";
+
 		public xamarin_geolocationPage()
 		{
+			GetLocation();
 			InitializeComponent();
+		}
+
+		public async void GetLocation()
+		{
+			try
+			{
+				var locator = CrossGeolocator.Current;
+				locator.DesiredAccuracy = 50;
+
+				var position = await locator.GetPositionAsync(10000);
+				if (position == null)
+					return;
+
+				this.lblLatitude.Text = string.Format("Latitude: {0}", position.Latitude);
+				this.lblLongitude.Text = string.Format("Longitude: {0}", position.Longitude);
+
+				Debug.WriteLine("Position Status: {0}", position.Timestamp);
+				Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+				Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
+			}
 		}
 	}
 }
